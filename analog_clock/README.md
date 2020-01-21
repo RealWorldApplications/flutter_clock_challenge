c05776eb7a097827a3fa89242ffefc41eb1c77d2
carol <carol.bolger@gmail.com>
2020-01-20 23:42:48 -0500

    added assets

--- analog_clock/README.md
+++ analog_clock/README.md
@@ -1,10 +1,6 @@
 # Analog Clock
 
 This app is an example of an analog clock.
-It has a light theme and a dark theme, and displays sample weather and location data.
-
-<img src='analog.gif' width='350'>
-
-<img src='analog_dark.png' width='350'>
-
-<img src='analog_light.png' width='350'>
+It has a gradient background that changes with
+the weather condition.
+The weather icon fades in and out for a subtle animation.
--- analog_clock/lib/analog_clock.dart
+++ analog_clock/lib/analog_clock.dart
@@ -4,13 +4,14 @@
 
 import 'dart:async';
 
+import 'package:flutter/rendering.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:vector_math/vector_math_64.dart' show radians;
 
-import 'container_hand.dart';
 import 'drawn_hand.dart';
 
 /// Total distance traveled by a second or a minute hand, each second or minute,
@@ -32,12 +33,24 @@ class AnalogClock extends StatefulWidget {
   _AnalogClockState createState() => _AnalogClockState();
 }
 
-class _AnalogClockState extends State<AnalogClock> {
+class _AnalogClockState extends State<AnalogClock>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+
   var _now = DateTime.now();
   var _temperature = '';
   var _temperatureRange = '';
   var _condition = '';
   var _location = '';
+  var _textColor = Color(0xFF000000);
+  var _secondHandColor = Color(0xFF000000);
+  var _hourMinuteHandColor = Color(0xFF000000);
+  var _gradientstart = Color(0xFF223344);
+  var _gradientend = Color(0xFF446688);
+
+  String _weatherIcon = 'assets/sunny.svg';
+
   Timer _timer;
 
   @override
@@ -47,6 +60,17 @@ class _AnalogClockState extends State<AnalogClock> {
     // Set the initial values.
     _updateTime();
     _updateModel();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    )..addListener(() => setState(() {}));
+
+    animation = Tween<double>(
+      begin: 0.2,
+      end: 1.0,
+    ).animate(animationController);
+
+    animationController.repeat();
   }
 
   @override
@@ -71,7 +95,83 @@ class _AnalogClockState extends State<AnalogClock> {
       _temperatureRange = '(${widget.model.low} - ${widget.model.highString})';
       _condition = widget.model.weatherString;
       _location = widget.model.location;
+      _updateImage();
     });
+  }
+
+  void _updateImage() {
+    switch (_condition) {
+      case "cloudy":
+        {
+          _textColor = Color(0xFF011E42);
+          _gradientstart = Color(0xFFE7F1F3);
+          _gradientend = Color(0xFF80909D);
+          _weatherIcon = 'assets/cloudy.svg';
+          _secondHandColor = Color(0xB4151515);
+          _hourMinuteHandColor = Color(0xFF151515);
+        }
+        break;
+      case "foggy":
+        {
+          _textColor = Color(0xFFFFFFF5);
+          _gradientstart = Color(0xFF828282);
+          _gradientend = Color(0xFF4A545E);
+          _weatherIcon = 'assets/foggy.svg';
+          _secondHandColor = Color(0xB4151515);
+          _hourMinuteHandColor = Color(0xFF151515);
+        }
+        break;
+      case "rainy":
+        {
+          _textColor = Color(0xFFCECECE);
+          _gradientstart = Color(0xFF828282);
+          _gradientend = Color(0xFF111255);
+          _weatherIcon = 'assets/rainy.svg';
+          _secondHandColor = Color(0xB4151515);
+          _hourMinuteHandColor = Color(0xFF151515);
+        }
+        break;
+
+      case "snowy":
+        {
+          _textColor = Color(0xFF015808);
+          _gradientstart = Color(0xFFFFFAFA);
+          _gradientend = Color(0xFFE5F3FB);
+          _weatherIcon = 'assets/snowy.svg';
+          _secondHandColor = Color(0xB4151515);
+          _hourMinuteHandColor = Color(0xFF151515);
+        }
+        break;
+      case "sunny":
+        {
+          _textColor = Color(0xFF151515);
+          _gradientstart = Color(0xFFFFDF00);
+          _gradientend = Color(0xFFFF7A03);
+          _weatherIcon = 'assets/sunny.svg';
+          _secondHandColor = Color(0xB4151515);
+          _hourMinuteHandColor = Color(0xFF151515);
+        }
+        break;
+      case "thunderstorm":
+        {
+          _textColor = Color(0xFFFAF2F2);
+          _gradientstart = Color(0xFF202223);
+          _gradientend = Color(0xFF001C3D);
+          _weatherIcon = 'assets/thunderstorm.svg';
+          _secondHandColor = Color(0xB4CECECE);
+          _hourMinuteHandColor = Color(0xFFCECECE);
+        }
+        break;
+      case "windy":
+        {
+          _textColor = Color(0xFF070505);
+          _gradientstart = Color(0xFFE1CD9D);
+          _gradientend = Color(0xFFA79E88);
+          _weatherIcon = 'assets/windy.svg';
+          _secondHandColor = Color(0xB4151515);
+          _hourMinuteHandColor = Color(0xFF151515);
+        }
+    }
   }
 
   void _updateTime() {
@@ -79,6 +179,7 @@ class _AnalogClockState extends State<AnalogClock> {
       _now = DateTime.now();
       // Update once per second. Make sure to do it at the beginning of each
       // new second, so that the clock is accurate.
+
       _timer = Timer(
         Duration(seconds: 1) - Duration(milliseconds: _now.millisecond),
         _updateTime,
@@ -88,90 +189,140 @@ class _AnalogClockState extends State<AnalogClock> {
 
   @override
   Widget build(BuildContext context) {
-    // There are many ways to apply themes to your clock. Some are:
-    //  - Inherit the parent Theme (see ClockCustomizer in the
-    //    flutter_clock_helper package).
-    //  - Override the Theme.of(context).colorScheme.
-    //  - Create your own [ThemeData], demonstrated in [AnalogClock].
-    //  - Create a map of [Color]s to custom keys, demonstrated in
-    //    [DigitalClock].
-    final customTheme = Theme.of(context).brightness == Brightness.light
-        ? Theme.of(context).copyWith(
-            // Hour hand.
-            primaryColor: Color(0xFF4285F4),
-            // Minute hand.
-            highlightColor: Color(0xFF8AB4F8),
-            // Second hand.
-            accentColor: Color(0xFF669DF6),
-            backgroundColor: Color(0xFFD2E3FC),
-          )
-        : Theme.of(context).copyWith(
-            primaryColor: Color(0xFFD2E3FC),
-            highlightColor: Color(0xFF4285F4),
-            accentColor: Color(0xFF8AB4F8),
-            backgroundColor: Color(0xFF3C4043),
-          );
-
     final time = DateFormat.Hms().format(DateTime.now());
-    final weatherInfo = DefaultTextStyle(
-      style: TextStyle(color: customTheme.primaryColor),
+
+    final dateInfo = DefaultTextStyle(
+      style: TextStyle(color: _textColor),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(_temperature),
-          Text(_temperatureRange),
-          Text(_condition),
-          Text(_location),
+          Text(
+            DateFormat.yMd().format(_now),
+            textScaleFactor: 2.0,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            DateFormat(widget.model.is24HourFormat ? 'Hms' : 'jms')
+                .format(_now),
+            textScaleFactor: 1.5,
+            style: TextStyle(fontWeight: FontWeight.normal),
+          ),
         ],
       ),
     );
 
+    final tempInfo = DefaultTextStyle(
+      style: TextStyle(color: _textColor),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            _temperature,
+            textScaleFactor: 2.0,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            _temperatureRange,
+            textScaleFactor: 1.5,
+            style: TextStyle(fontWeight: FontWeight.normal),
+          ),
+        ],
+      ),
+    );
+
+    final locationInfo = DefaultTextStyle(
+      style: TextStyle(color: _textColor),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            _location,
+            textScaleFactor: 2.0,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
     return Semantics.fromProperties(
       properties: SemanticsProperties(
         label: 'Analog clock with time $time',
         value: time,
       ),
       child: Container(
-        color: customTheme.backgroundColor,
+        decoration: BoxDecoration(
+            gradient: RadialGradient(
+          colors: [_gradientstart, _gradientend],
+        )),
         child: Stack(
           children: [
-            // Example of a hand drawn with [CustomPainter].
-            DrawnHand(
-              color: customTheme.accentColor,
-              thickness: 4,
-              size: 1,
-              angleRadians: _now.second * radiansPerTick,
-            ),
-            DrawnHand(
-              color: customTheme.highlightColor,
-              thickness: 16,
-              size: 0.9,
-              angleRadians: _now.minute * radiansPerTick,
-            ),
-            // Example of a hand drawn with [Container].
-            ContainerHand(
-              color: Colors.transparent,
-              size: 0.5,
-              angleRadians: _now.hour * radiansPerHour +
-                  (_now.minute / 60) * radiansPerHour,
-              child: Transform.translate(
-                offset: Offset(0.0, -60.0),
-                child: Container(
-                  width: 32,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    color: customTheme.primaryColor,
-                  ),
+            Positioned(
+              left: 10,
+              top: 5,
+              child: FadeTransition(
+                opacity: animation,
+                child: SvgPicture.asset(
+                  _weatherIcon,
+                  color: _textColor,
+                  height: 60,
                 ),
               ),
             ),
             Positioned(
-              left: 0,
-              bottom: 0,
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: weatherInfo,
+              left: 20,
+              top: 80,
+              child: Text(
+                _condition,
+                textScaleFactor: 1.2,
+                style:
+                    TextStyle(fontWeight: FontWeight.normal, color: _textColor),
               ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  DateFormat.EEEE().format(_now),
+                  textScaleFactor: 2.5,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: _textColor,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 10,
+              top: 5,
+              child: dateInfo,
+            ),
+            DrawnHand(
+              color: _secondHandColor,
+              thickness: 4,
+              size: .8,
+              angleRadians: _now.second * radiansPerTick,
+            ),
+            DrawnHand(
+              color: _hourMinuteHandColor,
+              thickness: 8,
+              size: 0.6,
+              angleRadians: _now.minute * radiansPerTick,
+            ),
+            DrawnHand(
+              color: _hourMinuteHandColor,
+              thickness: 16,
+              size: 0.4,
+              angleRadians: _now.hour * radiansPerTick,
+            ),
+            Positioned(
+              left: 10,
+              bottom: 5,
+              child: tempInfo,
+            ),
+            Positioned(
+              right: 10,
+              bottom: 5,
+              child: locationInfo,
             ),
           ],
         ),
